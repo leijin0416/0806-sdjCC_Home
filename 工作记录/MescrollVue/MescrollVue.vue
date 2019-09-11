df1226ed6f2e212da8619816f7cc0d984a991972
leijin0416 <1427780404@qq.com>
2019-09-11 20:11:51 +0800

    提交代码

--- 工作记录/MescrollVue/MescrollVue.vue
+++ 工作记录/MescrollVue/MescrollVue.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="page">
+    <div class="page" id="searchBar">
         <mescroll-vue 
             ref="mescroll" 
             :down="mescrollDown" 
@@ -11,21 +11,24 @@
                 :HouseList="articleList" />
 
         </mescroll-vue>
+        <!-- 返回顶部 -->
+        <div class="m-top-img"
+            id="toTop" 
+            :class="searchBarFixed == true ? 'isFixed' :''"
+            @click="searchBarClick">
+            <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1568196620635&di=fd19064dc428e841bc5fd51aee4177ff&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic2%2Fcover%2F00%2F31%2F21%2F5810a9d3c190f_610.jpg" alt="top.png" class="img">
+        </div>
     </div>
 </template>
 
 <script>
-import MescrollVue from 'mescroll.js/mescroll.vue'  //引用插件
 import axios from '@/common/api/http.js'
+import MescrollVue from 'mescroll.js/mescroll.vue'
 
-import ScreenList from '@/components/ScreenList/ScreenList'
+import ScreenList from '@/components/sassAnimation/sassAnimation'
 
 export default {
-    name: '',
-    components: {
-		'm-list': ScreenList,
-        MescrollVue // 注册mescroll组件
-    },
+    name: 'mescroll',
     data() {
         return {
             mescroll: null, 
@@ -33,39 +36,28 @@ export default {
             mescrollUp: { 
                 callback: this.upCallback, // 上拉回调
                 page: {
-                    num: 0, //当前页 默认0,
-                    size: 10 //每页数据条数,默认10
+                    num: 0, //当前页 默认0,回调之前会加1; 
+                    size: 10 //每页数据条数
                 },
-                htmlNodata: '<p class="upwarp-nodata">-- 我是有底线的 --</p>',
                 noMoreSize: 5, 
-                toTop: {
-                    //回到顶部按钮
-                    src: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1568112696184&di=191290f61e6130a5929bdd6289a7cf9a&imgtype=0&src=http%3A%2F%2Fpic.51yuansu.com%2Fpic3%2Fcover%2F00%2F65%2F15%2F5895b559e9620_610.jpg", 
-                    offset: 600,
-                    warpClass: 'mescroll-totop'
-                },
-                htmlContent: '<p class="downwarp-progress"></p><p class="downwarp-tip">下拉刷新 </p>', 
-				empty: {
-					//列表第一页无任何数据时,显示的空提示布局; 需配置warpId才显示
-					warpId: "xxid", //父布局的id (1.3.5版本支持传入dom元素)
-					icon: "./static/mescroll/mescroll-empty.png", //图标,默认null,支持网络图
-					tip: "暂无相关数据~" //提示
-				}
+                htmlContent: '<p class="downwarp-progress"></p><p class="downwarp-tip">下拉刷新</p>', 
+                htmlNodata: '<p class="upwarp-nodata">-- 我是有底线的 --</p>',
+                isBounce: true,
             },
             articleList: [], // 列表数据
 
 			Pagetotal: 0,//总条数
 			Pagesize: 5,//每页显示多少条
-			historyData: [],//分页数据的盒子
+            historyData: [],//分页数据的盒子
+            
+            searchBarFixed: false,
         }
     },
-    //监控data中的数据变化
-    watch: {
-
-    },
-    //监听属性 类似于data概念
-    computed: {
-
+    props:{
+        step:{ //此数据是控制动画快慢的
+            type:Number,
+            default:50
+        }
     },
     beforeRouteEnter (to, from, next) { 
         next(vm => {
@@ -79,6 +71,16 @@ export default {
         // 退出路由时,记录列表滚动的位置,隐藏回到顶部按钮和isBounce的配置
         next()
     },
+    components: {
+		'm-list': ScreenList,
+        MescrollVue // 注册mescroll组件
+    },
+    //监听属性 类似于data概念
+    computed: {
+        
+    },
+    //监控data中的数据变化
+    watch: {},
     //方法集合
     methods: {
         mescrollInit(mescroll) {
@@ -114,12 +116,12 @@ export default {
                             
                         }
                         /**
-                         *  获取当前总页数 -手动分页
+                         *  获取当前总页数,取整数
                          */
                         let page=Math.ceil(this.Pagetotal/this.Pagesize);
                         
                         /**
-                         *  数据渲染成功后, 最后一页隐藏下拉刷新的状态， 后台接口有返回列表的总页数 page
+                         *  数据渲染成功后, 隐藏下拉刷新的状态， 后台接口有返回列表的总页数 page
                          */
                         this.$nextTick(() => {
                             mescroll.endByPage(this.Pagetotal, page);
@@ -131,12 +133,11 @@ export default {
             }, 2000);
         },
 		/**
-         *  changePages -手动分页
-         * 
-         *      - 因为每次滑动到底部都会触发 upCallback 访问接口的事件，所以用changePages 事件来做分页；
-         *      - houses 为每次滑动后的当前数据，然后通过for循环遍历里面的数据，最后通过 push 将数据添加到 articleList数组中；
-         *      - 
-         */ 
+         *  changePages 手动控制分页
+         *  - 因为每次滑动到底部都会触发 upCallback 访问接口的事件，所以用changePages做分页
+         *  - houses 为每次滑动的不同数据，然后通过for循环遍历里面的数据，最后通过 push 添加到 articleList数组中
+         *  - 
+         */
 		changePages(index) {
             let _start = ( index - 1 ) * this.Pagesize;
             let _end = index * this.Pagesize;
@@ -144,35 +145,87 @@ export default {
 
             for (let i = 0; i < houses.length; i++) {
                 let element = houses[i];
-                this.articleList.push(element);  //将每次拿到的新数据 push添加到 articleList 数组中
+                this.articleList.push(element);  //将每次拿到的新数据存储到articleList中
 
             }
                 
             // console.log(this.articleList);
         },
         OnshowInfoClick() {},
+        /**
+         *  显示返回按钮
+         */
+        handleScroll () {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+            let offsetTop = document.querySelector('#searchBar').offsetTop;
+
+            if (scrollTop > offsetTop) {
+                this.searchBarFixed = true;
+            } else {
+                this.searchBarFixed = false;
+                //this.$emit('onSearchBarFixed', false)
+            }
+            // console.log(scrollTop);
+            // console.log(offsetTop);
+            
+        },
+        /**
+         *  返回动画
+         */
+        searchBarClick() {
+            let top = document.documentElement.scrollTop || document.body.scrollTop;
+            // 实现滚动效果 
+            const timeTop = setInterval(() => {
+                document.body.scrollTop = document.documentElement.scrollTop = top -= 50;
+                if (top <= 0) {
+                    clearInterval(timeTop);
+                }
+            }, 10);
+            //window.animate({scrollTop:0},500);
+        },
     },
     //生命周期 - 创建完成（可以访问当前this实例）
     created() {},
     //生命周期 - 挂载完成（可以访问DOM元素）
-    mounted() {},
+    mounted() {
+        // 设置监听
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed () {
+        // 移除监听
+        window.removeEventListener('scroll', this.handleScroll)
+    },
 }
 </script>
 
 <style lang="scss" scoped>
-/*@import url(); 引入公共css类*/
+/*@import url(); 引入公共css类
+*/
 .page {
-    overflow-y: auto;
-}
-// 设定高度
-.mescroll {
-    -webkit-overflow-scrolling:touch;
-    // position: absolute;
-    // top: 262px;
-    // bottom: 0;
-    // height: auto;
-}
-.mescroll-totop {
-    
+    position: relative;
+    background-color: #fff;
+    .m-top-img {
+        z-index: 999;
+        position: fixed;
+        bottom: 70px;
+        right: 15px;
+        opacity: 0;
+        transition: all .4s linear;
+        .img {
+            width: 45px;
+            height: 45px;
+            overflow: hidden;
+            border-radius: 50%;
+            box-shadow: 0 4px 6px #ddd;
+            border: 6px solid #f6f6f6;
+            background-color: #f6f6f6;
+        }
+    }
+    .isFixed {
+        opacity: 1;
+    }
+    .mescroll {
+        -webkit-overflow-scrolling:touch;
+    }
 }
 </style>
--- 工作记录/MescrollVue/README.md
+++ 工作记录/MescrollVue/README.md
@@ -1,19 +1,85 @@
 #### 移动端上拉加载和下拉刷新的vue插件： mescroll.js
 
-```js
+- 1、完成上拉加载；
+- 2、完成图标返回顶部；
 
-npm install --save mescroll.js //不要使用cnpm安装
+```
+
+npm install --save mescroll.js      //不要使用cnpm安装
+
+外部在引用 MescrollVue 组件时，也可以通过 <router-view></router-view> 的形式展示出来；
 
 ```
 
 文档配置： http://www.mescroll.com/api.html#options
+
 参考： https://www.jianshu.com/p/dfabe1661738?utm_source=oschina-app
 
 ScreenList（m-list） 为数据的组件，MescrollVue 为加载的组件；
 
-外部在引用 MescrollVue 组件时，也可以通过 <router-view></router-view> 的形式展示出来；
+---
 
-```css
+#### 手动分页：
+```
+<script>
+
+    let pageHouseList = res.data.data.houses;
+    
+    // 分页-保存取到的所有数据到数组中
+    this.historyData = res.data.data.houses;
+    this.Pagetotal = res.data.data.houses.length; //获取数组长度
+    // 初始化显示，小于每页显示条数，全部显示，大于每页显示条数，取前每页条数显示
+    if(res.data.data.houses.length < this.Pagesize){
+        this.articleList = this.historyData;
+
+    } else{
+        if (num === 1) {
+            this.articleList = this.historyData.slice(0,this.Pagesize);
+        } else {
+            this.changePages(num);
+        }
+        
+    }
+
+    --------------
+
+    
+    changePages(index) {
+        let _start = ( index - 1 ) * this.Pagesize;
+        let _end = index * this.Pagesize;
+        let houses = this.historyData.slice(_start, _end);
+
+        for (let i = 0; i < houses.length; i++) {
+            let element = houses[i];
+            this.articleList.push(element);  //将每次拿到的新数据存储到articleList中
+
+        }
+    },
+
+<script>
+```
+
+#### 当数据都加载完成后，会隐藏刷新的状态：
+
+```
+<script>
+
+    /**
+        *  获取当前总页数,取整数
+        */
+    let page=Math.ceil(this.Pagetotal/this.Pagesize);
+    
+    /**
+        *  数据渲染成功后, 隐藏下拉刷新的状态， 后台接口有返回列表的总页数 page
+        */
+    this.$nextTick(() => {
+        mescroll.endByPage(this.Pagetotal, page);
+    });
+
+</script>
+```
+#### 优化滑动效果：
+```
 // 设定高度
 .mescroll {
     //-webkit-overflow-scrolling:touch;
@@ -25,3 +91,45 @@ ScreenList（m-list） 为数据的组件，MescrollVue 为加载的组件；
 
 ```
 当 position: fixed; 时返回顶部（toTop）才会生效；
+
+---
+
+#### 引入postcss-px-to-viewport插件 （vw适配）：
+
+```
+    npm install postcss-loader postcss-px-to-viewport --save
+```
+
+在vue.config.js文件中配置即可使用：
+```
+    css: {
+		extract: true, 
+        loaderOptions: {
+            sass: {
+                data: `
+                @import "@/style/mixin.scss";
+                @import "@/style/_var.scss";
+                `
+            },
+            postcss: {
+                plugins: [
+                    require("postcss-px-to-viewport")({
+                        unitToConvert: "px",
+                        viewportWidth: 750,
+                        unitPrecision: 5,
+                        propList: [ "*" ],
+                        viewportUnit: "vw",
+                        fontViewportUnit: "vw",
+                        selectorBlackList: [],
+                        minPixelValue: 1,
+                        mediaQuery: false,
+                        replace: true,
+                        exclude: /(\/|\\)(node_modules)(\/|\\)/,
+                    })
+                ]
+            },
+        },
+    },
+```
+
+参考文章：https://www.jianshu.com/p/9af556e0f2fc

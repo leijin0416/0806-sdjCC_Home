0973f388c86d3f8ad8fea4117a07d5e4bf76f461
leijin0416 <1427780404@qq.com>
2019-09-12 00:57:32 +0800

    更新代码

---
+++ 工作记录/正则条件/README.md
@@ -0,0 +1,15 @@
+#### 正则验证
+
+ - 使用 (模块中引入)：
+     import {regs, regex, reEmail, myreg, regNum} from '@/assets/js/regExps.js' .
+
+ - 判断：
+     !regNum.test(showKeyValue)   //不为纯数字时
+
+```js
+
+( num * value ).toFixed(4);     -四舍五入
+
+parseFloat(this.otcList.sell_lave_count);   -转换为数值
+
+```
--- 工作记录/正则条件/regExps.js
+++ 工作记录/正则条件/regExps.js
@@ -1,16 +1,3 @@
-/**
- *      ( num * value ).toFixed(4);     -四舍五入
- *      parseFloat(this.otcList.sell_lave_count);   -转换为数值
- * 
- *      正则验证
- *      -使用：
- *          import {regs, regex, reEmail, myreg, regNum} from '@/assets/js/regExps.js'  // 模块中引入
- *      
- *      -判断
- *          !regNum.test(showKeyValue)   //不为纯数字时
- * 
- * ************************************************ */
-
  /**
   *  特殊字符
   */
